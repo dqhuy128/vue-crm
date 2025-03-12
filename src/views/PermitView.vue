98682b4ad0b53a3b7795efa69a883da99cc19951
duongta <duongtac22@gmail.com>
2025-03-12 22:20:56 +0700

    init

---
+++ src/views/PermitView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MainLayout from "./MainLayout.vue"
+</script>
+
+<template>
+  <MainLayout>
+    <Buttons :title="`active`" />
+    <slot />
+  </MainLayout>
+</template>
