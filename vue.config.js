c892780e2e62e4ed226e315fe49a02e93ab3d6ce
Quang Huy Exclusive <huydaoquang@bizfly.vn>
2025-03-29 13:44:10 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,6 @@ module.exports = {
         additionalData: `@import "@/assets/sass/style.scss"`
       }
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-crm/" : "/"
 }
