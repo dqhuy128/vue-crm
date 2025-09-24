1c48f21b91598c1d0e6b848db902c5464e63a566
Quang Huy exclusive <quanghuy_exclusive@gmail.com>
2025-09-25 00:41:55 +0700

    update

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -13,11 +13,7 @@
 
             <div class="flex-1 pe-6">
               <p class="text-[16px] leading-6 text-[#1b4dea] italic">
-                {{
-                  bio ||
-                  dataInformation?.slogan ||
-                  'Yêu tổ quốc, yêu đồng bào, học tập tốt, lao động tốt, giữ vệ sinh thật tốt'
-                }}
+                {{ bio || dataInformation?.slogan || 'Chưa có slogan' }}
               </p>
 
               <div class="mt-4 grid gap-y-3 border-t border-[#EDEDF6] pt-4 md:grid-cols-2">
