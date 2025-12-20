051c063f1dfebc9d5a9262d82cc527f7a524f6e1
Quang Huy exclusive <quanghuy_exclusive@gmail.com>
2025-12-21 02:29:54 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'vue-auth3/drivers/auth/bearer.js': path.resolve(__dirname, 'node_modules/vue-auth3/drivers/auth/bearer.js'),
     },
   },
 })
